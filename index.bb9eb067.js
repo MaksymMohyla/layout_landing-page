041f818281b40606456a01c2f99e93ba873e9f2a
Maksym Mohyla <greenparksquad@gmail.com>
2024-07-19 17:58:38 +0300

    make build

---
+++ index.bb9eb067.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("buttonGoTop");window.addEventListener("scroll",function(){document.body.scrollTop>100||document.documentElement.scrollTop>100?e.style.display="block":e.style.display="none"}),e.addEventListener("click",function(){document.documentElement.scrollTop=0});
+//# sourceMappingURL=index.bb9eb067.js.map
